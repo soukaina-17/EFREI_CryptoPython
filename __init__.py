08fbd4a37fcbec050d4b7848f99df57babcc5812
soukaina <soukaina.ljaouhari@gmail.com>
2025-02-21 11:37:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask import render_template
 from flask import json
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
+      #comm                                                                                                                                 
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
